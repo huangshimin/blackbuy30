8e4a04c8b92b2b8eafd872be20416d161de4cc56
Your Name <you@example.com>
2019-04-30 17:18:02 +0800

    首页与详情页数据渲染

--- src/App.vue
+++ src/App.vue
@@ -38,9 +38,11 @@
                 <div id="menu2" class="nav-box menuhd">
                     <ul>
                         <li class="index">
-                            <a href="#" class="">
+                            <!-- <a href="#" class=""> -->
+                              <router-link to="/index">
                                 <span class="out" style="top: 0px;">首页</span>
-                            </a>
+                            <!-- </a> -->
+                            </router-link>
                         </li>
                         <li class="news">
                             <a href="#" class="">
@@ -80,6 +82,9 @@
             </div>
         </div>
     </div>
+
+    <!-- 出口 -->
+    <router-view></router-view>
 
     <!-- 底部 -->
 
--- src/main.js
+++ src/main.js
@@ -1,10 +1,45 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//导入路由
+import VueRouter from 'vue-router'
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+
+Vue.use(ElementUI);
+
+Vue.use(VueRouter)
+
+//写组件
+import index from './components/index.vue'
+import detail from './components/detail.vue'
+
+
+//写规则
+const routes = [
+  {
+    path:'/index',
+    component:index
+  },
+  {
+    path:'/detail/:id',
+    component:detail
+  }
+]
+
+//实例化路由对象
+const router = new VueRouter({
+  routes
+})
+
 import './assets/statics/site/css/style.css'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  //挂载大奥vue实例上
+  router
 }).$mount('#app')
